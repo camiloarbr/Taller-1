7906b2d74ed5106b3d36d3248e665d274d38b885
Camilo <Carbelaezr@eafit.edu.co>
2025-02-09 13:33:11 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Camilo Arbelaez'})
+    return render(request, 'home.html', {'name': 'Camilo Arbelaez Restrepo'})
 
 def about(request):
     return HttpResponse("About Page")
