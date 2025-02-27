f27f45f12ecadd53b0dc052dcb4a6b2465f61e82
Camilo <Carbelaezr@eafit.edu.co>
2025-02-27 11:13:07 -0500

    Base de datos de movies

--- movie/models.py
+++ movie/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=250)  
+    description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+    genre = models.CharField(max_length=100, default='Not specified')
+    year = models.PositiveIntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.title 
